2238b6eae4acb3697c26db0ce3c6b7c253524a30
sch246 <980001119@qq.com>
2025-10-30 19:23:21 +0800

    1.20.1 bugfix
    java.lang.RuntimeException: Attempted to load class net/minecraft/client/gui/screens/Screen for invalid dist DEDICATED_SERVER
    以及其它小修改

--- src/main/java/com/sch246/muhc/MaidUseHandCrank.java
+++ src/main/java/com/sch246/muhc/MaidUseHandCrank.java
@@ -4,34 +4,13 @@ import com.sch246.muhc.config.ClothConfigIntegration;
 import com.sch246.muhc.create.InitPoi;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -51,7 +30,9 @@ public class MaidUseHandCrank {
         if (!FMLEnvironment.production) {
             LOGGER.info("[{}] Registering config screen...", MODID);
         }
-        ClothConfigIntegration.registerConfigScreen();
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            ClothConfigIntegration.registerConfigScreen();
+        }
     }
 
 }
