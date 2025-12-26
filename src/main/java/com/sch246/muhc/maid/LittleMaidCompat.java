bdeb02d7faf8f6c2de16976129d6ed0d4ee1caff
sch246 <980001119@qq.com>
2025-12-26 19:00:03 +0800

    同步修复

--- src/main/java/com/sch246/muhc/maid/LittleMaidCompat.java
+++ src/main/java/com/sch246/muhc/maid/LittleMaidCompat.java
@@ -5,7 +5,9 @@ import com.github.tartaricacid.touhoulittlemaid.api.LittleMaidExtension;
 import com.github.tartaricacid.touhoulittlemaid.entity.task.TaskManager;
 import com.sch246.muhc.event.ClothConfigEvent;
 import com.sch246.muhc.maid.task.HandCrankTask;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.common.NeoForge;
 
 
@@ -13,7 +15,7 @@ import net.neoforged.neoforge.common.NeoForge;
 public class LittleMaidCompat implements ILittleMaid {
     // 默认构造函数，女仆模组会在合适的时间调用这个构造函数。可以在这里注册女仆专属的事件
     public LittleMaidCompat() {
-        if (ModList.get().isLoaded("cloth_config")) {
+        if (FMLEnvironment.dist == Dist.CLIENT && ModList.get().isLoaded("cloth_config")) {
             NeoForge.EVENT_BUS.register(new ClothConfigEvent());
         }
     }
